3fb82cb43b78df66795bdb76dd50936a4d2e62e2
Guyp <gu.yp@qq.com>
2023-03-02 22:45:32 +0800

    add lspsapa

--- lua/configs/lspsaga.lua
+++ lua/configs/lspsaga.lua
@@ -1,2 +1,39 @@
 require("lspsaga").setup({})
 
+local keymap = vim.keymap.set
+
+-- LSP finder - Find the symbol's definition
+-- If there is no definition, it will instead be hidden
+-- When you use an action in finder like "open vsplit",
+-- you can use <C-t> to jump back
+keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
+-- Code action
+
+-- Diagnostic jump
+-- You can use <C-o> to jump back to your previous location
+keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
+
+-- Diagnostic jump with filters such as only jumping to an error
+keymap("n", "[E", function()
+  require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+end)
+keymap("n", "]E", function()
+  require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
+end)
+
+-- Toggle outline 函数列表
+keymap("n","<leader>o", "<cmd>Lspsaga outline<CR>")
+
+-- Hover Doc
+-- If there is no hover doc,
+-- there will be a notification stating that
+-- there is no information available.
+-- To disable it just use ":Lspsaga hover_doc ++quiet"
+-- Pressing the key twice will enter the hover window
+keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+keymap("n", "K", "<cmd>Lspsaga hover_doc ++keep<CR>")
+
+-- Call hierarchy 调用层次
+keymap("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
+keymap("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
--- lua/core/init.lua
+++ lua/core/init.lua
@@ -9,7 +9,7 @@ require("configs.lualine") --状态栏
 require("configs.nvim-tree") --文档树
 
 require("configs.treesitter") -- 语法高亮
-require("configs.indentline") -- 语法高亮
+require("configs.indentline") -- 缩进线
 require("configs.lsp") --lsp 
 require("configs.handlers").setup() --lsp 
 --require("configs.lspsaga") --lsp ui
@@ -22,7 +22,7 @@ require("configs.autopairs") --自动括号
 
 require("configs.bufferline") --buffer分割
 require("configs.gitsigns") --左侧git提示
-require("configs.telescope") -- 文件检索
+require("configs.telescope") -- 通知
 require("configs.notity") -- 文件检索
-require("configs.toggleterm") -- 文件检索
+require("configs.toggleterm") -- 终端
 
